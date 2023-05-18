d3feec0b51f7610837e8492acaed6d3ddb7da207
Aman Patel☕ <aman17626@gmail.com>
2023-05-18 23:04:12 +0530

    fix - minor bug fixes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
-menu.onClick = () => {
+menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
 };
